5eb6b36c60c1209ec0e30569f8ecce906839021a
peter <peter.bogar@pandora.sk>
2021-07-12 16:35:49 +0200

    comments

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Main script
-# TODO: comments
-# TODO: risk free rate
+# 1. download daily data for selected stocks
+# 2. calculating performance data if holding one stock from each: corelation, total return, avg annual return, volatility, max drawdown, Sharpe ratio
+# TODO: table output to excel
+# TODO: warning if there is not data for some symbol
 
 import pandas as pd
 import datetime as dt
@@ -17,34 +19,53 @@ from portfolio_functions import stock_get_perfromance
 # Display all columns in pandas dataframe
 pd.set_option('display.max_columns', None)
 
-
+# Initial variables
 initial_account = 10000
-start_date = '2010-01-01'
-end_date = '2020-12-31'
+start_date = '2019-01-01'
+end_date = '2021-07-01'
 symbols = ['AAPL', 'QQQ', 'GLD', 'TLT', 'SPY']
 
 
 if __name__ == '__main__':
 
+    # Time period in years to download data
     time_period = (dt.datetime.strptime(end_date, '%Y-%m-%d') - dt.datetime.strptime(start_date, '%Y-%m-%d')).days / 365
 
+    # Downloading data for each stock
     df_data_download = download(symbols, start_date, end_date)
     print(df_data_download.head())
+
+    # Calculating correlation among stocks
     df_corr = df_data_download.corr().round(2)
+
+    # Calculating return for each stocks
     df_stock_return = stock_return(symbols, df_data_download)
+
+    # Calculating volatility for each stocks
     df_stock_volatility = stock_volatility(symbols, df_stock_return)
+
+    # Calculating Sharpe ratio for enach stocks
     df_stock_sharpe_ratio = stock_sharpe_ratio(symbols, time_period, df_stock_volatility)
+
+    # Calculating drawdown for single stocks
     df_stock_drawdown = stock_drawdown(symbols, df_stock_sharpe_ratio)
+
+    # Write results to a file
     # df_stock_drawdown.to_excel('output.xlsx')
 
+    # Table output
     print()
     print('Stock Correlation:')
     print(tabulate(df_corr, headers='keys', tablefmt='fancy_grid'))
     print()
     print('Stock performance:')
+    print('From date: ', start_date)
+    print('To date: ', end_date)
+    print(round(time_period, 2), 'years')
     perf = stock_get_perfromance(symbols, time_period, df_stock_drawdown)
     print(tabulate(perf, headers='keys', tablefmt='fancy_grid'))
 
+    # Chart output
     # df_chart = df_stock_drawdown[['AAPL_cumulative_gain%', 'AAPL_drawdown%']]
     # df_chart.plot()
     # plt.show()
--- portfolio_functions.py
+++ portfolio_functions.py
@@ -1,16 +1,20 @@
-# Functions
+# Functions for Portfolio analysis
 
 import yfinance as yf
 import pandas as pd
 
 
 def download(symbols, start_date, end_date):
-    # Function to download close price for symbols for specified date window from Yahoo
-    # Usage: download(['AAPL', 'SPY'], '2020-01-01', '2020-12-31')
+    # Function to download close price for symbols for specified date window from Yahoo or from file
+    # Input: list of symbols, start date, end date
+    # Output: dataframe with close price for each day
+    # Usage: download(list of symbols, '2020-01-01', '2020-12-31')
 
     # Read data from Yahoo
     df = yf.download(symbols, start_date, end_date)
+    # Read only 'Adj price' for each symbol
     df_data_download = df[[("Adj Close", s) for s in symbols]]
+    # Remove two level header
     df_data_download.columns = df_data_download.columns.droplevel(level=0)
     # df_close_prices.to_csv('downloaded_data.csv')
 
@@ -22,10 +26,11 @@ def download(symbols, start_date, end_date):
 
 def stock_return(symbols, df_stock_return):
     # Function to calculate return for 1 long stock for each symbol
+    # Input: list of symbols, dataframe with close prices
+    # Output: to input dataframe added total absolut return and total % return
+    # Usage: stock_return(list of symbols, df_data_download)
 
     for symbol in symbols:
-        # print(df_stock_return[symbol].values[0])
-        # print(df_stock_return[symbol].values[-1])
         df_stock_return[symbol + '_cumulative_gain'] = df_stock_return[symbol] - df_stock_return[symbol].values[0]
         df_stock_return[symbol + '_cumulative_gain%'] = (df_stock_return[symbol + '_cumulative_gain'] / df_stock_return[symbol].values[0])
 
@@ -33,8 +38,10 @@ def stock_return(symbols, df_stock_return):
 
 
 def stock_volatility(symbols, df_stock_volatility):
-    # Function for calculate volatility of returns for single stock
-    # volatility = std(daily_gain) * sqrt(252)
+    # Function for calculate annualized volatility of returns for each symbol
+    # Input: list of symbols, dataframe with close prices
+    # Output: to input dataframe add daily gain and its volatility: volatility = std(daily_gain) * sqrt(252)
+    # Usage: stock_volatility(list of symbols, df_stock_return)
 
     for symbol in symbols:
         df_stock_volatility[symbol + '_daily_gain'] = df_stock_volatility[symbol]/df_stock_volatility[symbol].shift(1) - 1
@@ -47,9 +54,12 @@ def stock_volatility(symbols, df_stock_volatility):
 
 
 def stock_sharpe_ratio(symbols, time_period, df_stock_sharpe_ratio):
-    # Function to calculating Sharpe ratio for 1 stock
-    # sharpe ratio = (return - risk free return) / volatility
+    # Function to calculating Sharpe ratio for each stock: sharpe ratio = (return/time period - risk free return) / volatility
+    # Input: symbols, time period, dataframe with gain in and volatility for each stock
+    # Output: to input dataframe add Sharpe ratio
+    # Usage: stock_sharpe_ratio(list of symbols, time_period, df_stock_volatility)
 
+    # Risk free rate is set to 3%, TODO: opravit
     risk_free_rate = 0.03
 
     for symbol in symbols:
@@ -59,8 +69,10 @@ def stock_sharpe_ratio(symbols, time_period, df_stock_sharpe_ratio):
 
 
 def stock_drawdown(symbols, df_stock_drawdown):
-    # Function to calculate max drawdown
-    # drawdown = (current price/highest price so far) -1
+    # Function to calculate max drawdown for each stock: drawdown = (current price/highest price so far) -1
+    # Input: list of symbols, dataframe with close prices
+    # Output: to input dataframe add max drawdown (one number)
+    # Usage: stock_drawdown(symbols, df_stock_sharpe_ratio)
 
     for symbol in symbols:
         df_stock_drawdown[symbol + '_highest_price'] = df_stock_drawdown[symbol].cummax()
@@ -72,7 +84,11 @@ def stock_drawdown(symbols, df_stock_drawdown):
 
 def stock_get_perfromance(symbols, time_period, df_stock_drawdown):
     # Function to collect performance data for each stock from raw dataframe
+    # Input: symbols, time period (in years), dataframe with cumulative return, volatility, max drawdown, sharpe ratio for each symbol
+    # Output: dataframe with columns (avg annual return, volatility, max drawdown, sharpe ratio) and rows (each symbol)
+    # Usage: stock_get_perfromance(symbols, time_period, df_stock_drawdown)
 
+    # Defining output dataframe
     df_stock_performance = pd.DataFrame({'Avg Annual Return %': [''], 'Volatility %': [''], 'Max Drawdown %': [''], 'Sharpe Ratio': ['']}, index=symbols)
 
     for symbol in symbols:
